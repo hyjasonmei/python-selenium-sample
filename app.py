1ea43dc9f10265ef488bde3fb14efafb4763c70d
Jason Mei <s0922380@gmail.com>
2022-03-11 15:53:54 +0800

    change to only one thread one time to avoid chrome session problem

--- app.py
+++ app.py
@@ -10,24 +10,26 @@ from uuid import uuid4
 import time
 import base64
 import json
+import os
+
+os.system('taskkill /im chromedriver.exe /F')
+os.system('taskkill /im chrome.exe /F')
 
 app = Flask(__name__)
-app.debug = True
-
 url_base = "http://127.0.0.1:5000/"
+chrome_options = Options()
+# chrome_options.add_argument("--disable-extensions")
+# chrome_options.add_argument("--disable-gpu")
+# #chrome_options.add_argument("--no-sandbox") # linux only
+# chrome_options.add_argument("--headless")
+# chrome_options.headless = True # also works
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+driver.set_window_size(1920,1080)
+driver.set_page_load_timeout(5)
 
 @app.route("/", methods=['POST'])
 def get_image():
     try:
-        chrome_options = Options()
-        chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument("--disable-gpu")
-        #chrome_options.add_argument("--no-sandbox") # linux only
-        chrome_options.add_argument("--headless")
-        # chrome_options.headless = True # also works
-        driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-        driver.set_window_size(1920,1080)
-        driver.set_page_load_timeout(5)
         encoded_data = base64.b64encode(json.dumps(request.json).encode('utf-8'))
         url = "{}?data={}".format(url_base, encoded_data.decode('utf-8'))
         # print(url)
@@ -37,15 +39,13 @@ def get_image():
         # im = Image.open(imageStream)
         # path = "./files/{}.png".format(uuid4())
         # im.save(path)
-        # time.sleep(3)
+        # time.sleep(5)
         return send_file(imageStream, as_attachment=True, mimetype='image/png', download_name='{}.png'.format(uuid4()))
     except Exception as e:
         print(e)
-    finally:
-        driver.close()
-    
+
     return "Render failed", 400
     
 
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', debug=False, port=5003)
+    app.run(host='0.0.0.0', debug=False, port=5003, threaded=False, processes=1)
